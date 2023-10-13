c2e96b3e169a3ad8be17956ae721cd92cb160d18
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2023-10-13 16:32:41 -0300

    Created the classes LineGrid and CheckerGrid to implement the interface IGridGenerator. The implementation is still in progress.

---
+++ PixelArtEditor/PixelArtEditor/Grids/CheckerGrid.cs
@@ -0,0 +1,21 @@
+﻿namespace PixelArtEditor.Grids
+{
+    internal class CheckerGrid : IGridGenerator
+    {
+        Bitmap checkerGridPiece = new(1, 1);
+        public void GenerateGrid(Bitmap originalImage, int cellSize, Color gridColor)
+        {
+            checkerGridPiece = new Bitmap(originalImage.Width, originalImage.Height);
+        }
+
+        public Bitmap ApplyGridFullImage(Bitmap originalImage)
+        {
+            return originalImage;
+        }
+
+        public Bitmap ApplyGridSinglePixel(Bitmap originalImage)
+        {
+            return originalImage;
+        }
+    }
+}
---
+++ PixelArtEditor/PixelArtEditor/Grids/LineGrid.cs
@@ -0,0 +1,23 @@
+﻿namespace PixelArtEditor.Grids
+{
+    internal class LineGrid : IGridGenerator
+    {
+        Bitmap lineGridPiece = new(1, 1);
+
+        public void GenerateGrid(Bitmap originalImage, int cellSize, Color gridColor)
+        {
+            lineGridPiece = new Bitmap(originalImage.Width, originalImage.Height);
+        }
+
+        public Bitmap ApplyGridFullImage(Bitmap originalImage)
+        {
+
+            return originalImage;
+        }
+
+        public Bitmap ApplyGridSinglePixel(Bitmap originalImage)
+        {
+            return originalImage;
+        }
+    }
+}
