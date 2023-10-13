3084b4c17af350d752e2bfd1b37dc8c541091519
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2023-10-13 16:18:14 -0300

    Created a Grid Generator Interface.

---
+++ PixelArtEditor/PixelArtEditor/Grids/IGridGenerator.cs
@@ -0,0 +1,11 @@
+﻿namespace PixelArtEditor.Grids
+{
+    internal interface IGridGenerator
+    {
+        public void GenerateGrid(Bitmap originalImage, int cellSize, Color gridColor);
+
+        public Bitmap ApplyGridFullImage(Bitmap originalImage);
+
+        public Bitmap ApplyGridSinglePixel(Bitmap originalImage);
+    }
+}
