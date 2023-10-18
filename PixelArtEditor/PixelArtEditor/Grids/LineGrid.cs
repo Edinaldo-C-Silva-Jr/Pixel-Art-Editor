6c2f7444707caa46524fd1df9e27074411904ee6
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2023-10-18 14:57:44 -0300

    Implemented the LineGrid class, with methods to generate and apply a line grid to an image.

--- PixelArtEditor/PixelArtEditor/Grids/LineGrid.cs
+++ PixelArtEditor/PixelArtEditor/Grids/LineGrid.cs
@@ -3,14 +3,32 @@
     internal class LineGrid : IGridGenerator
     {
         Bitmap lineGridPiece = new(1, 1);
-        Bitmap lineGridPixel = new(1, 1);
+        Bitmap lineGridSinglePixel = new(1, 1);
 
         public void GenerateGrid(Bitmap originalImage, int cellSize, Color gridColor)
         {
             int gridPieceWidth = DefineGridPieceSize(originalImage.Width / cellSize);
             int gridPieceHeight = DefineGridPieceSize(originalImage.Height / cellSize);
 
-            lineGridPiece = new Bitmap(originalImage.Width, originalImage.Height);
+            lineGridPiece = new Bitmap(gridPieceWidth, gridPieceHeight);
+            lineGridPiece.MakeTransparent();
+
+            using Pen gridPen = new(gridColor);
+            using Graphics gridBuilder = Graphics.FromImage(lineGridPiece);
+
+            for (int x = 1; x < gridPieceHeight / cellSize + 1; x++)
+            {
+                gridBuilder.DrawLine(gridPen, 0, cellSize * x - 1, gridPieceHeight, cellSize * x - 1);
+            }
+            for (int y = 1; y < gridPieceWidth / cellSize + 1; y++)
+            {
+                gridBuilder.DrawLine(gridPen, cellSize * y - 1, 0, cellSize * y - 1, gridPieceWidth);
+            }
+
+            lineGridSinglePixel = new Bitmap(cellSize, cellSize);
+            using Graphics gridPixelBuilder = Graphics.FromImage(lineGridSinglePixel);
+            gridPixelBuilder.DrawLine(gridPen, 0, cellSize - 1, cellSize - 1, cellSize - 1);
+            gridPixelBuilder.DrawLine(gridPen, cellSize - 1, 0, cellSize - 1, cellSize - 1);
         }
 
         private int DefineGridPieceSize(int sizePixelLength)
@@ -20,6 +38,10 @@
             for (int i = 1; i < Math.Sqrt(sizePixelLength) + 1; i++)
             {
                 gridPieceSize = sizePixelLength / i;
+                if (sizePixelLength % i != 0)
+                {
+                    gridPieceSize++;
+                }
 
                 if (sizePixelLength % gridPieceSize != 0)
                 {
@@ -30,7 +52,7 @@
                     amountOfGridPieces = (int)Math.Pow(sizePixelLength / gridPieceSize, 2);
                 }
 
-                int totalIterations = amountOfGridPieces + gridPieceSize*2;
+                int totalIterations = amountOfGridPieces + gridPieceSize * 2;
                 if (totalIterations < amountOfIterations)
                 {
                     amountOfIterations = totalIterations;
@@ -43,12 +65,21 @@
 
         public Bitmap ApplyGridFullImage(Bitmap originalImage)
         {
-
+            using Graphics gridMerger = Graphics.FromImage(originalImage);
+            for (int y = 0; y < originalImage.Height / lineGridPiece.Height; y++)
+            {
+                for (int x = 0; x < originalImage.Width / lineGridPiece.Width; x++)
+                {
+                    gridMerger.DrawImage(lineGridPiece, lineGridPiece.Width * x, lineGridPiece.Height * y);
+                }
+            }
             return originalImage;
         }
 
         public Bitmap ApplyGridSinglePixel(Bitmap originalImage, int xPosition, int yPosition)
         {
+            using Graphics lineGridPixelMerger = Graphics.FromImage(originalImage);
+            lineGridPixelMerger.DrawImage(lineGridSinglePixel, xPosition, yPosition);
             return originalImage;
         }
     }
