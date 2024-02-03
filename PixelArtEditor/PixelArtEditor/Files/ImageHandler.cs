98600e3e42c532f92d02ff7891e09f889216f0cc
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-02-03 19:53:31 -0300

    Implemented some methods to the ImageHandler class to allow manipulating the image (Still more to come). This will replace all the references to the image in the main form.

--- PixelArtEditor/PixelArtEditor/Files/ImageHandler.cs
+++ PixelArtEditor/PixelArtEditor/Files/ImageHandler.cs
@@ -12,16 +12,58 @@
         /// </summary>
         public Bitmap ClipboardImage { get; set; }
 
+        public Rectangle SelectedArea { get; set; }
+
+        public Point SelectionStart { get; set; }
+
+        private SolidBrush SelectionBrush { get; set; }
+
         public ImageHandler()
         {
+            SelectionBrush = new(Color.FromArgb(128, 32, 196, 255));
             OriginalImage = new(1, 1);
             ClipboardImage = new(1, 1);
+        }
+
+        public void CreateNewImage(int width, int height, int zoom, Color backgroundColor, bool transparent)
+        {
+            // Creates the image
+            OriginalImage = new(width * zoom, height * zoom);
+            using Graphics imageFiller = Graphics.FromImage(OriginalImage);
+            imageFiller.Clear(backgroundColor);
+
+            // Changes transparency
+            if (transparent)
+            {
+                OriginalImage.MakeTransparent(backgroundColor);
+            }
+        }
+
+        // TODO: maybe try to make this more efficient (and have less parameters)
+        public void ChangeImageTransparency(Color oldColor, Color newColor, bool transparent, int width, int height, int zoom, Color gridColor, Color backgroundColor)
+        {
+            OriginalImage.MakeTransparent(oldColor);
+
+            if (!transparent)
+            {
+                Bitmap temporaryImage = new(width * zoom, height * zoom);
+                Graphics temporaryGraphics = Graphics.FromImage(temporaryImage);
+                temporaryGraphics.Clear(newColor);
+                temporaryGraphics.DrawImage(OriginalImage, 0, 0);
+                OriginalImage = temporaryImage;
+            }
+        }
+
+        public void ClearSelection()
+        {
+            SelectedArea = Rectangle.Empty;
         }
 
         public void Dispose()
         {
             OriginalImage?.Dispose();
             ClipboardImage?.Dispose();
+            SelectionBrush?.Dispose();
         }
     }
 }
