e3f78fe4ae8465f2f61eff671a2371d2917467fe
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-08-21 22:36:36 -0300

    Removed the calculation methods from DrawingTool and put them in a helper class.

--- PixelArtEditor/PixelArtEditor/Image Editing/Drawing Tools/DrawingTool.cs
+++ PixelArtEditor/PixelArtEditor/Image Editing/Drawing Tools/DrawingTool.cs
@@ -1,4 +1,6 @@
-﻿namespace PixelArtEditor.Drawing_Tools
+﻿using PixelArtEditor.Image_Editing;
+
+namespace PixelArtEditor.Drawing_Tools
 {
     /// <summary>
     /// A base implementation of a Drawing Tool, that has some of the common methods used by most other tools.
@@ -14,7 +16,7 @@
         /// <param name="pixelSize">The size of each pixel in the image.</param>
         protected static void DrawPixel(Graphics drawGraphics, SolidBrush drawBrush, Point location, int pixelSize)
         {
-            Point pixelLocation = SnapPixelTopLeft(location, pixelSize);
+            Point pixelLocation = DrawingCalculations.SnapPixelTopLeft(location, pixelSize);
             drawGraphics.FillRectangle(drawBrush, pixelLocation.X, pixelLocation.Y, pixelSize, pixelSize);
         }
 
@@ -42,21 +44,8 @@
         /// <param name="rectangleHeight">The height of the rectangle, in pixel sizes.</param>
         protected static void DrawRectangle(Graphics drawGraphics, SolidBrush drawBrush, Point location, int pixelSize, int rectangleWidth, int rectangleHeight)
         {
-            Point pixelLocation = SnapPixelTopLeft(location, pixelSize);
+            Point pixelLocation = DrawingCalculations.SnapPixelTopLeft(location, pixelSize);
             drawGraphics.FillRectangle(drawBrush, pixelLocation.X, pixelLocation.Y, pixelSize * rectangleWidth, pixelSize * rectangleHeight);
-        }
-
-        /// <summary>
-        /// Receives a location and snaps it to the top left of a pixel. This makes sure the changes are always applied to the entire pixel.
-        /// </summary>
-        /// <param name="absoluteLocation">The actual location of the mouse click.</param>
-        /// <param name="pixelSize">The size of each pixel in the image.</param>
-        /// <returns>The new location that indicates the top left corner of the current pixel.</returns>
-        protected static Point SnapPixelTopLeft(Point absoluteLocation, int pixelSize)
-        {
-            int xPos = absoluteLocation.X - Modulo(absoluteLocation.X, pixelSize);
-            int yPos = absoluteLocation.Y - Modulo(absoluteLocation.Y, pixelSize);
-            return new(xPos, yPos);
         }
 
         /// <summary>
@@ -91,53 +80,6 @@
             
             colorBrush = new(previewColor);
             return colorBrush;
-        }
-
-        /// <summary>
-        /// Receives two coordinates, an initial coordinate and a final one, and compares them.
-        /// If the final coordinate is smaller than the initial, they're swapped.
-        /// This method accepts a single coordinate, which can be the X or Y coordinate of a Point.
-        /// </summary>
-        /// <param name="initialCoordinate">The coordinate from the starting point.</param>
-        /// <param name="finalCoordinate">The coordinate from the end point.</param>
-        /// <returns>A tuple containing both coordinates in the correct order.</returns>
-        protected static (int, int) SwapCoordinatesWhenStartIsBigger(int initialCoordinate, int finalCoordinate)
-        {
-            if (initialCoordinate > finalCoordinate)
-            {
-                return (finalCoordinate, initialCoordinate);
-            }
-            else
-            {
-                return (initialCoordinate, finalCoordinate);
-            }
-        }
-
-        /// <summary>
-        /// Custom modulo function that correctly handles negative values, returning a positive remainder.
-        /// </summary>
-        /// <param name="value">The value that will have its remainder returned.</param>
-        /// <param name="modulo">The number to divide the value and get the remainder.</param>
-        /// <returns>The remainder of the division between a value and a number.</returns>
-        protected static int Modulo(int value, int modulo)
-        {
-            return (value % modulo + modulo) % modulo;
-        }
-
-        /// <summary>
-        /// Calculates and returns the distance between two points in pixel sizes.
-        /// This method accepts a single coordinate, which can be the X or Y coordinate of a Point.
-        /// </summary>
-        /// <param name="initialCoordinate">The coordinate of the first mouse click.</param>
-        /// <param name="finalCoordinate">The coordinate of the current mouse position.</param>
-        /// <param name="pixelSize">The size of each pixel in the image.</param>
-        /// <returns>The distance between the two coordinates, expressed in pixel sizes.</returns>
-        protected static int GetDistanceInPixelSizes(int initialCoordinate, int finalCoordinate, int pixelSize)
-        {
-            initialCoordinate -= Modulo(initialCoordinate, pixelSize); // The starting coordinate is the left or up edge of the pixel.
-            finalCoordinate = finalCoordinate - (Modulo(finalCoordinate, pixelSize)) + pixelSize; // The ending coordinate is the left or up edge of the next pixel.
-
-            return Math.Abs((finalCoordinate - initialCoordinate) / pixelSize);
         }
 
         abstract public void UseToolClick(Graphics imageGraphics, SolidBrush colorBrush, OptionalToolParameters toolParameters);
---
+++ PixelArtEditor/PixelArtEditor/Image Editing/DrawingCalculations.cs
@@ -0,0 +1,65 @@
+﻿namespace PixelArtEditor.Image_Editing
+{
+    public static class DrawingCalculations
+    {
+        /// <summary>
+        /// Custom modulo function that correctly handles negative values, returning a positive remainder.
+        /// </summary>
+        /// <param name="value">The value that will have its remainder returned.</param>
+        /// <param name="modulo">The number to divide the value and get the remainder.</param>
+        /// <returns>The remainder of the division between a value and a number.</returns>
+        public static int Modulo(int value, int modulo)
+        {
+            return (value % modulo + modulo) % modulo;
+        }
+
+        /// <summary>
+        /// Receives a location and snaps it to the top left of a pixel. This makes sure the changes are always applied to the entire pixel.
+        /// </summary>
+        /// <param name="absoluteLocation">The actual location of the mouse click.</param>
+        /// <param name="pixelSize">The size of each pixel in the image.</param>
+        /// <returns>The new location that indicates the top left corner of the current pixel.</returns>
+        public static Point SnapPixelTopLeft(Point absoluteLocation, int pixelSize)
+        {
+            int xPos = absoluteLocation.X - Modulo(absoluteLocation.X, pixelSize);
+            int yPos = absoluteLocation.Y - Modulo(absoluteLocation.Y, pixelSize);
+            return new(xPos, yPos);
+        }
+
+        /// <summary>
+        /// Receives two coordinates, an initial coordinate and a final one, and compares them.
+        /// If the final coordinate is smaller than the initial, they're swapped.
+        /// This method accepts a single coordinate, which can be the X or Y coordinate of a Point.
+        /// </summary>
+        /// <param name="initialCoordinate">The coordinate from the starting point.</param>
+        /// <param name="finalCoordinate">The coordinate from the end point.</param>
+        /// <returns>A tuple containing both coordinates in the correct order.</returns>
+        public static (int, int) SwapCoordinatesWhenStartIsBigger(int initialCoordinate, int finalCoordinate)
+        {
+            if (initialCoordinate > finalCoordinate)
+            {
+                return (finalCoordinate, initialCoordinate);
+            }
+            else
+            {
+                return (initialCoordinate, finalCoordinate);
+            }
+        }
+
+        /// <summary>
+        /// Calculates and returns the distance between two points in pixel sizes.
+        /// This method accepts a single coordinate, which can be the X or Y coordinate of a Point.
+        /// </summary>
+        /// <param name="initialCoordinate">The coordinate of the first mouse click.</param>
+        /// <param name="finalCoordinate">The coordinate of the current mouse position.</param>
+        /// <param name="pixelSize">The size of each pixel in the image.</param>
+        /// <returns>The distance between the two coordinates, expressed in pixel sizes.</returns>
+        public static int GetDistanceInPixelSizes(int initialCoordinate, int finalCoordinate, int pixelSize)
+        {
+            initialCoordinate -= Modulo(initialCoordinate, pixelSize); // The starting coordinate is the left or up edge of the pixel.
+            finalCoordinate = finalCoordinate - (Modulo(finalCoordinate, pixelSize)) + pixelSize; // The ending coordinate is the left or up edge of the next pixel.
+
+            return Math.Abs((finalCoordinate - initialCoordinate) / pixelSize);
+        }
+    }
+}
