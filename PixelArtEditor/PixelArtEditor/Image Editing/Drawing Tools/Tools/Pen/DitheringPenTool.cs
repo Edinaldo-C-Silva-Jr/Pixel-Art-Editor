0498568d5345d2cfd52478a48d93335c9f7d9017
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-09-08 13:16:20 -0300

    Changed the DitheringPenTool to define which pixels to draw based ont he first click. Also fixed the preview.

--- PixelArtEditor/PixelArtEditor/Image Editing/Drawing Tools/Tools/Pen/DitheringPenTool.cs
+++ PixelArtEditor/PixelArtEditor/Image Editing/Drawing Tools/Tools/Pen/DitheringPenTool.cs
@@ -1,11 +1,12 @@
-﻿
-namespace PixelArtEditor.Image_Editing.Drawing_Tools.Tools.Pen
+﻿namespace PixelArtEditor.Image_Editing.Drawing_Tools.Tools.Pen
 {
     public class DitheringPenTool : BasePenTool
     {
+        private byte? Parity { get; set; } = null;
+
         protected override void DrawPenPixel(Graphics drawGraphics, SolidBrush drawBrush, Point location)
         {
-            if (location.X % 2 + location.Y % 2 == 1)
+            if (Parity is not null && (location.X % 2 + location.Y % 2) % 2 == Parity.Value)
             {
                 drawGraphics.FillRectangle(drawBrush, location.X, location.Y, 1, 1);
             }
@@ -13,11 +14,26 @@ namespace PixelArtEditor.Image_Editing.Drawing_Tools.Tools.Pen
 
         protected override void DrawPenPixel(Graphics drawGraphics, SolidBrush drawBrush, Point location, int zoom)
         {
-            if (location.X % 2 + location.Y % 2 == 1)
+            if (Parity is null || (location.X % 2 + location.Y % 2) % 2 == Parity)
             {
                 location = new(location.X * zoom, location.Y * zoom);
                 drawGraphics.FillRectangle(drawBrush, location.X, location.Y, zoom, zoom);
             }
+        }
+
+        public override void UseToolClick(Bitmap drawingImage, Color drawingColor, OptionalToolParameters toolParameters)
+        {
+            if (toolParameters.ClickLocation.HasValue)
+            {
+                Parity = (byte)((toolParameters.ClickLocation.Value.X % 2 + toolParameters.ClickLocation.Value.Y % 2) % 2);
+                base.UseToolClick(drawingImage, drawingColor, toolParameters);
+            }
+        }
+
+        public override void UseToolRelease(OptionalToolParameters toolParameters)
+        {
+            Parity = null;
+            base.UseToolRelease(toolParameters);
         }
     }
 }
