ad2e10543dc267c29c8c81e750f73c3a22339ea1
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-09-26 14:11:22 -0300

    Added comments to the changes in the OrdinalLineTool.

--- PixelArtEditor/PixelArtEditor/Image Editing/Drawing Tools/Tools/Line/OrdinalLineTool.cs
+++ PixelArtEditor/PixelArtEditor/Image Editing/Drawing Tools/Tools/Line/OrdinalLineTool.cs
@@ -6,9 +6,22 @@
     /// </summary>
     public class OrdinalLineTool : CardinalLineTool
     {
+        #region Properties
+        /// <summary>
+        /// Indicates whether the line will be diagonal or cardinal.
+        /// </summary>
         private bool LineIsDiagonal { get; set; }
+
+        /// <summary>
+        /// Indicates if the line is pointing to the right or to the left.
+        /// </summary>
         private bool LinePointsRight { get; set; }
+
+        /// <summary>
+        /// Indicates if the line is pointing down or up.
+        /// </summary>
         private bool LinePointsDown { get; set; }
+        #endregion
 
         protected override void DrawLine(Graphics drawGraphics, SolidBrush drawBrush)
         {
@@ -56,29 +69,36 @@
             }
         }
 
+        /// <summary>
+        /// Checks the direction that the line is pointing.
+        /// This checks if the line is diagonal, if it's pointing right and if it's pointing down.
+        /// </summary>
+        /// <param name="horizontalDifference">The absolute distance between the X position of the starting and ending points.</param>
+        /// <param name="verticalDifference">The absolute distance between the Y position of the starting and ending points.</param>
         private void CheckLineDirections(int horizontalDifference, int verticalDifference)
         {
             // Checks whether the line extends further horizontally or vertically.
-            bool lineIsHorizontal = horizontalDifference > verticalDifference;
+            bool lineGoesFurtherHorizontally = horizontalDifference > verticalDifference;
             // The line will be diagonal if the bigger difference is smaller than twice the smaller difference.
-            LineIsDiagonal = (lineIsHorizontal && horizontalDifference < 2 * verticalDifference) || (!lineIsHorizontal && verticalDifference < 2 * horizontalDifference);
+            LineIsDiagonal = (lineGoesFurtherHorizontally && horizontalDifference < 2 * verticalDifference) 
+                || (!lineGoesFurtherHorizontally && verticalDifference < 2 * horizontalDifference);
             // Defines the directions the line points towards.
             LinePointsRight = StartingPoint!.Value.X < EndPoint!.Value.X;
             LinePointsDown = StartingPoint!.Value.Y < EndPoint!.Value.Y;
         }
 
-
         /// <summary>
-        /// Draws a diagonal line. The direction and size of the line are defined by the parameters.
+        /// Draws a diagonal line. The direction of the line is defined by the LinePointsRight and LinePointsDown properties.
+        /// The line is drawn one pixel at a time, shifting the position by 1 pixel horizontally and vertically to make it diagonal.
         /// </summary>
-        /// <param name="graphics">The graphics to draw the pixel on.</param>
-        /// <param name="brush">The brush to use when drawing the pixel.</param>
+        /// <param name="graphics">The graphics for the image being drawn.</param>
+        /// <param name="brush">The brush with the currently selected color.</param>
         /// <param name="firstPoint">The starting point of the line.</param>
-        /// <param name="pixelSize">The size of a pixel in the image.</param>
-        /// <param name="lineLength">The length of the line in pixel sizes.</param>
-        /// <param name="lineDirection">The direction of the line. This can be 0 = Left Up, 1 = Left Down, 2 = Right Up and 3 = Right Down.</param>
+        /// <param name="lastPoint">The ending point of the line.</param>
+        /// <param name="zoom">The size of each pixel in the image.</param>
         private void DrawDiagonalLine(Graphics graphics, SolidBrush brush, Point firstPoint, Point lastPoint, int zoom)
         {
+            // Line points right/down.
             if (LinePointsRight && LinePointsDown)
             {
                 for (int i = 0; i < lastPoint.X - firstPoint.X + zoom; i += zoom)
@@ -86,6 +106,7 @@
                     graphics.FillRectangle(brush, firstPoint.X + i, firstPoint.Y + i, zoom, zoom);
                     EndPoint = new(firstPoint.X + i, firstPoint.Y + i);
 
+                    // If the last point reaches the right or bottom of the image, stop drawing the line.
                     if (firstPoint.X + i == UneditedImage!.Height - 1 || firstPoint.Y + i == UneditedImage!.Width - 1)
                     {
                         break;
@@ -93,6 +114,7 @@
                 }
             }
 
+            // Line points right/up.
             if (LinePointsRight && !LinePointsDown)
             {
                 for (int i = 0; i < lastPoint.X - firstPoint.X + zoom; i += zoom)
@@ -100,6 +122,7 @@
                     graphics.FillRectangle(brush, firstPoint.X + i, firstPoint.Y - i, zoom, zoom);
                     EndPoint = new(firstPoint.X + i, firstPoint.Y - i);
 
+                    // If the last point reaches the right or top of the image, stop drawing the line.
                     if (firstPoint.X + i == UneditedImage!.Height - 1 || firstPoint.Y - i == 0)
                     {
                         break;
@@ -107,6 +130,7 @@
                 }
             }
 
+            // Line points left/down.
             if (!LinePointsRight && LinePointsDown)
             {
                 for (int i = 0; i < firstPoint.X - lastPoint.X + zoom; i += zoom)
@@ -114,6 +138,7 @@
                     graphics.FillRectangle(brush, firstPoint.X - i, firstPoint.Y + i, zoom, zoom);
                     EndPoint = new(firstPoint.X - i, firstPoint.Y + i);
 
+                    // If the last point reaches the left or bottom of the image, stop drawing the line.
                     if (firstPoint.X - i == 0 || firstPoint.Y + i == UneditedImage!.Width - 1)
                     {
                         break;
@@ -121,6 +146,7 @@
                 }
             }
 
+            // Line points left/up.
             if (!LinePointsRight && !LinePointsDown)
             {
                 for (int i = 0; i < firstPoint.X - lastPoint.X + zoom; i += zoom)
@@ -128,6 +154,7 @@
                     graphics.FillRectangle(brush, firstPoint.X - i, firstPoint.Y - i, zoom, zoom);
                     EndPoint = new(firstPoint.X - i, firstPoint.Y - i);
 
+                    // If the last point reaches the left or top of the image, stop drawing the line.
                     if (firstPoint.X - i == 0 || firstPoint.Y - i == 0)
                     {
                         break;
