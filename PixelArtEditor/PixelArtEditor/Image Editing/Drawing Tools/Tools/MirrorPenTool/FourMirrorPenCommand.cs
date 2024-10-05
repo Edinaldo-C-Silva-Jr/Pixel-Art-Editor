bc85fe1b271bd1df861477523bacd86185c49996
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2024-10-05 15:57:30 -0300

    Added a FourMirrorPenCommand and implemented the undo in the FourMirrorPenTool.

---
+++ PixelArtEditor/PixelArtEditor/Image Editing/Drawing Tools/Tools/MirrorPenTool/FourMirrorPenCommand.cs
@@ -0,0 +1,92 @@
+﻿using PixelArtEditor.Image_Editing.Undo_Redo;
+
+namespace PixelArtEditor.Image_Editing.Drawing_Tools.Tools.MirrorPenTool
+{
+    /// <summary>
+    /// A command that has the data and methods necessary to undo and redo actions made by the FourMirrorPen tool.
+    /// </summary>
+    public class FourMirrorPenCommand : IUndoRedoCommand
+    {
+        /// <summary>
+        /// The top left portion of the drawing image that was drawn on, before it was edited.
+        /// </summary>
+        private Bitmap TopLeftUneditedImage { get; }
+
+        /// <summary>
+        /// The top right portion of the drawing image that was drawn on, before it was edited.
+        /// </summary>
+        private Bitmap TopRightUneditedImage { get; }
+
+        /// <summary>
+        /// The bottom left portion of the drawing image that was drawn on, before it was edited.
+        /// </summary>
+        private Bitmap BottomLeftUneditedImage { get; }
+
+        /// <summary>
+        /// The bottom right portion of the drawing image that was drawn on, before it was edited.
+        /// </summary>
+        private Bitmap BottomRightUneditedImage { get; }
+
+        /// <summary>
+        /// The portion of the drawing image that was drawn on, after it was edited.
+        /// </summary>
+        private Bitmap EditedImage { get; }
+
+        /// <summary>
+        /// The four locations of the original image where the edit was done.
+        /// </summary>
+        private (Point topLeft, Point topRight, Point bottomLeft, Point bottomRight) EditLocations { get; }
+
+        /// <summary>
+        /// Default constructor. Receives the necessary data to create the command.
+        /// </summary>
+        /// <param name="topLeftOldImage">The top left part of the image that was drawn on, before it was edited.</param>
+        /// <param name="topRightOldImage">The top right part of the image that was drawn on, before it was edited.</param>
+        /// <param name="bottomLeftOldImage">The bottom left part of the image that was drawn on, before it was edited.</param>
+        /// <param name="bottomRightOldImage">The bottom right part of the image that was drawn on, before it was edited.</param>
+        /// <param name="newImage">The part of the image that was drawn on, after it was edited.</param>
+        /// <param name="topLeft">The location where the top left edit was done.</param>
+        /// <param name="topRight">The location where the top right edit was done.</param>
+        /// <param name="bottomLeft">The location where the bottom left edit was done.</param>
+        /// <param name="bottomRight">The location where the bottom right edit was done.</param>
+        public FourMirrorPenCommand(Bitmap topLeftOldImage, Bitmap topRightOldImage, Bitmap bottomLeftOldImage, Bitmap bottomRightOldImage, 
+            Bitmap newImage, Point topLeft, Point topRight, Point bottomLeft, Point bottomRight)
+        {
+            TopLeftUneditedImage = topLeftOldImage;
+            TopRightUneditedImage = topRightOldImage;
+            BottomLeftUneditedImage = bottomLeftOldImage;
+            BottomRightUneditedImage = bottomRightOldImage;
+            EditedImage = newImage;
+            EditLocations = (topLeft, topRight, bottomLeft, bottomRight);
+        }
+
+        public void ExecuteChange(Graphics imageGraphics)
+        {
+            // Draws the edited image on the top left edit point.
+            imageGraphics.DrawImage(EditedImage, EditLocations.topLeft);
+
+            using Bitmap mirroredImage = new(EditedImage);
+
+            // Flips the edited image horizontally and draws it on the top right edit point.
+            mirroredImage.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            imageGraphics.DrawImage(mirroredImage, EditLocations.topRight);
+            
+            // Flips the edited image horizontally/vertically and draws it on the bottom left edit point.
+            mirroredImage.RotateFlip(RotateFlipType.RotateNoneFlipXY);
+            imageGraphics.DrawImage(mirroredImage, EditLocations.bottomLeft);
+
+            // Flips the edited image horizontally and draws it on the bottom right edit point.
+            mirroredImage.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            imageGraphics.DrawImage(mirroredImage, EditLocations.bottomRight);
+        }
+
+        public void RollbackChange(Graphics imageGraphics)
+        {
+            // Draws each unedited image to their respective locations.
+            imageGraphics.DrawImage(TopLeftUneditedImage, EditLocations.topLeft);
+            imageGraphics.DrawImage(TopRightUneditedImage, EditLocations.topRight);
+            imageGraphics.DrawImage(BottomLeftUneditedImage, EditLocations.bottomLeft);
+            imageGraphics.DrawImage(BottomRightUneditedImage, EditLocations.bottomRight);
+        }
+    }
+}
--- PixelArtEditor/PixelArtEditor/Image Editing/Drawing Tools/Tools/MirrorPenTool/FourMirrorPenTool.cs
+++ PixelArtEditor/PixelArtEditor/Image Editing/Drawing Tools/Tools/MirrorPenTool/FourMirrorPenTool.cs
@@ -1,7 +1,42 @@
-﻿namespace PixelArtEditor.Image_Editing.Drawing_Tools.Tools.MirrorPenTool
+﻿using PixelArtEditor.Image_Editing.Undo_Redo;
+using System.Drawing.Imaging;
+
+namespace PixelArtEditor.Image_Editing.Drawing_Tools.Tools.MirrorPenTool
 {
     public class FourMirrorPenTool : BaseMirrorPenTool
     {
+        protected override void ValidateInitialLocation(int clickX, int clickY, Size imageSize)
+        {
+            // The click location will always be on the top and left since the other 3 quadrants will be mirrored.
+            if (clickX > (imageSize.Width - 1) / 2)
+            {
+                clickX = imageSize.Width - clickX - 1;
+            }
+
+            if (clickY > (imageSize.Height - 1) / 2)
+            {
+                clickY = imageSize.Height - clickY - 1;
+            }
+
+            base.ValidateInitialLocation(clickX, clickY, imageSize);
+        }
+
+        protected override void ValidateMirrorLocation(int clickX, int clickY, Size imageSize)
+        {
+            // The click location will always be on the top and left since the other 3 quadrants will be mirrored.
+            if (clickX > (imageSize.Width - 1) / 2)
+            {
+                clickX = imageSize.Width - clickX - 1;
+            }
+
+            if (clickY > (imageSize.Height - 1) / 2)
+            {
+                clickY = imageSize.Height - clickY - 1;
+            }
+
+            base.ValidateMirrorLocation(clickX, clickY, imageSize);
+        }
+
         protected override void DrawMirrorPixel(Graphics drawGraphics, SolidBrush drawBrush, Point location, Size imageSize)
         {
             // Draws the first pixel, in the click location.
@@ -38,6 +73,39 @@
             // Inverts the click location diagonally and draws another pixel.
             pixelPoint = new((imageSize.Width - location.X - 1) * zoom, (imageSize.Height - location.Y - 1) * zoom);
             drawGraphics.FillRectangle(drawBrush, pixelPoint.X, pixelPoint.Y, zoom, zoom);
+        }
+
+        public override IUndoRedoCommand CreateUndoStep(Point drawingImageLocation)
+        {
+            // Getting only the edited portion of the images.
+            // For a four mirror pen, the Edited Image only considers the top left quarter of the image, since the other quarters are the same image mirrored.
+            Rectangle editedArea = new(LeftBoundary, UpperBoundary, RightBoundary - LeftBoundary + 1, LowerBoundary - UpperBoundary + 1);
+            using Bitmap topLeftUneditedImage = UneditedImage!.Clone(editedArea, PixelFormat.Format32bppArgb);
+            EditedImage = EditedImage!.Clone(editedArea, PixelFormat.Format32bppArgb);
+
+            // Mirrors the area horizontally, to get the unedited portion of the image on the top right quarter.
+            editedArea = new(UneditedImage.Width - RightBoundary - 1, UpperBoundary, RightBoundary - LeftBoundary + 1, LowerBoundary - UpperBoundary + 1);
+            using Bitmap topRightUneditedImage = UneditedImage!.Clone(editedArea, PixelFormat.Format32bppArgb);
+
+            // Mirrors the area vertically, to get the unedited portion of the image on the bottom left quarter.
+            editedArea = new(LeftBoundary, UneditedImage.Height - LowerBoundary - 1, RightBoundary - LeftBoundary + 1, LowerBoundary - UpperBoundary + 1);
+            using Bitmap lowerLeftUneditedImage = UneditedImage!.Clone(editedArea, PixelFormat.Format32bppArgb);
+
+            // Mirrors the area horizontally and vertically, to get the unedited portion of the image on the bottom right quarter.
+            editedArea = new(UneditedImage.Width - RightBoundary - 1, UneditedImage.Height - LowerBoundary - 1, RightBoundary - LeftBoundary + 1, LowerBoundary - UpperBoundary + 1);
+            using Bitmap lowerRightUneditedImage = UneditedImage!.Clone(editedArea, PixelFormat.Format32bppArgb);
+
+            // Getting the locations where the edits started.
+            // These are the four locations that represent the top left pixel of all edited areas.
+            Point topLeftEditLocation = new(drawingImageLocation.X + LeftBoundary, drawingImageLocation.Y + UpperBoundary);
+            Point topRightEditLocation = new(drawingImageLocation.X + UneditedImage!.Width - RightBoundary - 1, drawingImageLocation.Y + UpperBoundary);
+            Point lowerLeftEditLocation = new(drawingImageLocation.X + LeftBoundary, drawingImageLocation.Y + UneditedImage.Height - LowerBoundary - 1);
+            Point lowerRightEditLocation = new(drawingImageLocation.X + UneditedImage!.Width - RightBoundary - 1, drawingImageLocation.Y + UneditedImage.Height - LowerBoundary - 1);
+
+            FourMirrorPenCommand undoStep = new(new Bitmap(topLeftUneditedImage), new Bitmap(topRightUneditedImage), new Bitmap(lowerLeftUneditedImage), new Bitmap(lowerRightUneditedImage), 
+                new(EditedImage), topLeftEditLocation, topRightEditLocation, lowerLeftEditLocation, lowerRightEditLocation);
+            ClearProperties();
+            return undoStep;
         }
     }
 }
